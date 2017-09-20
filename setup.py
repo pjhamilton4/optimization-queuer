7ec6134ad982df6b88504564c5f039fef7d1e289
Philip Hamilton <pjhamilton4@gmail.com>
2017-09-19 22:25:34 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+Testing for optimization-queuer 
+-------------
+
+Links
+`````
+
+"""
+import sys
+from setuptools import setup
+
+tests_require = [
+    'blinker'
+]
+
+
+setup(
+    name='optimization-tests'
+)
